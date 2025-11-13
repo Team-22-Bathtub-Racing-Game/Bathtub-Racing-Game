a7a3cdd25c5dcc061c68b5569f08355e947d2642
TreonnaB19 <trebg.22@gmail.com>
2025-11-12 21:22:09 -0500

    Added Timer & Lap Counter

---
+++ Assets/Scripts/Game/CountdownManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class CountdownManager : MonoBehaviour
+{
+    public TMP_Text countdownText;
+    public RaceTimer raceTimer;     // assign race timer
+    public PhysicsController playerKart;  // assign player's kart controller
+
+    void Start()
+    {
+        playerKart.canDrive = false;     // freeze kart
+        StartCoroutine(CountdownSequence());
+    }
+
+    IEnumerator CountdownSequence()
+    {
+        countdownText.text = "3";
+        yield return new WaitForSeconds(1f);
+
+        countdownText.text = "2";
+        yield return new WaitForSeconds(1f);
+
+        countdownText.text = "1";
+        yield return new WaitForSeconds(1f);
+
+        countdownText.text = "GO!";
+        raceTimer.StartRace();
+        playerKart.canDrive = true;      // UNFREEZE kart!
+        yield return new WaitForSeconds(1f);
+
+        countdownText.text = "";         // hide countdown
+    }
+}
